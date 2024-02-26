fecbe673f16dff192c859364a827890429e82a0e
Abinayasrinivasan305 <abinayasrinivasan2004@gmail.com>
2024-02-26 22:25:17 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express"; 
 import dotenv from "dotenv";//for this to do u need to moddify the package.json file and add heading "type":"module";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 
 
@@ -16,10 +17,16 @@ import { app, server } from "./socket/socket.js";
 
 const PORT=process.env.PORT || 5000;  //port variable value update process.env.port or either 5000 //easily can'get the value process.env.PORT first u require the package
 
+const __dirname= path.resolve();
+
 
 dotenv.config().parsed;
 
 app.use(express.json());                      //to parse the incoming requests (which is from auth.controller )  with JSON payloads (from req.body)
+
+app.get("^",(req,res) =>{
+    res.sendFile(path.join(__dirname,"/frontend/dist/index.html"))
+})
 
 //before routing this we call the cookie parser to access the cookies
 
@@ -28,6 +35,8 @@ app.use(cookieParser());
 app.use("/api/auth",authroutes);
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))    //this static given by express which store the static files like html,css,js
 
 
 
